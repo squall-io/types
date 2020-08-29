345b2ad95eef241ba4c4bdb1970b65099e684994
Salathiel Genese <salathiel@genese.name>
2020-08-29 21:49:48 +0100

    feat: build script & build docs at the same time

--- .postbuild.js
+++
@@ -1,7 +0,0 @@
-const replaceInFiles = require('replace-in-files');
-
-replaceInFiles({
-    from: /(declare type)/g,
-    to: 'export declare type',
-    files: './index.d.ts',
-}).catch(console.error);
