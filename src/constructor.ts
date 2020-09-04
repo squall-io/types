fe3785e4f2ed60730eaceba7ce181324cee3a30c
Salathiel Genese <salathiel@genese.name>
2020-09-04 07:03:06 +0100

    fix: documentation name clash

--- src/constructor.ts
+++ src/constructor.ts
@@ -25,7 +25,7 @@
  * }
  *
  * // Error: Type "string" is not assignable to type "number"
- * const MarsCoordinate: CoordinateConstructor = class {
+ * const NeptuneCoordinate: CoordinateConstructor = class {
  *     public longitude!: number;
  *     public latitude!: number;
  *
